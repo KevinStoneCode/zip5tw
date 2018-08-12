bfac626141b94203a8f22e8c3b487d7aca384c95
Kevin Stone <kwshih0212@gmail.com>
2018-08-12 19:46:42 +0800

    zip5 controller fix

--- app/controllers/zip5_controller.rb
+++ app/controllers/zip5_controller.rb
@@ -3,7 +3,7 @@ class Zip5Controller < ApplicationController
     @address = Address.new(params[:address])
     if not @address.scope.empty?
       excluded_side = @address.scope.to_i.odd? ? 'even' : 'odd'
-      @zip5_addresses = Zip5Address.where("city = ? and area = ? and road = ? and begin <= ? and end >= ? and side <> ?", @address.city, @address.area, @address.road, @address.scope, @address.scope, excluded_side)
+      @zip5_addresses = Zip5Address.where("city = ? and area = ? and road = ? and begin <= ? and zip5_addresses.end >= ? and side <> ?", @address.city, @address.area, @address.road, @address.scope, @address.scope, excluded_side)
     elsif not @address.road.empty?
       @zip5_addresses = Zip5Address.where("city = ? and area = ? and road = ? ", @address.city, @address.area, @address.road)
     else
